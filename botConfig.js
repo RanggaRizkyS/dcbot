91c72d329278c2c9e6fda078b6026c77b59e3784
meflems <89581469+meflems@users.noreply.github.com>
2022-03-22 07:32:59 +0530

    update botConfig.js

--- botConfig.js
+++ botConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: "OTUyODY3NDY1NjczNzE1ODAy.Yi8Q-w.qtggHMFpdDwV3NC5d4RuoI5lBUg",
+    token: "OTUyODY3NDY1NjczNzE1ODAy.Yi8Q-w.GD7d67z4x4Jrek2lVpI_z5S24aQ",
     prefix: "!",
     admins: [
         "739709686139584574"

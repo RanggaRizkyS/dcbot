9159520f1cbc17276b029264a0e06a80dcdc2500
meflems <89581469+meflems@users.noreply.github.com>
2022-03-23 22:42:52 +0530

    update index.js

--- index.js
+++ index.js
@@ -135,7 +135,7 @@ fs.readdir("./commands/", (err, files) => {
 client.login(process.env.TOKEN);
 require("http").createServer((_, res) => res.end("Acroga Online!")).listen(process.env.PORT || 8080)
 const guildsMany = {
-	"guilds": `${client.guilds.cache.size}`
+	"guilds": ${client.guilds.cache.size}
 };
 
 const data = JSON.stringify(guildsMany);

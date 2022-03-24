7e70b23ccfe9d0062b16d352ac195db977813e14
meflems <89581469+meflems@users.noreply.github.com>
2022-03-24 15:37:06 +0530

    update leaderboard.js

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -10,7 +10,7 @@ exports.execute = async (client, message, args) => {
         .setThumbnail(client.users.cache.get(leaderboard[0].id) ? client.users.cache.get(leaderboard[0].id).displayAvatarURL : "https://cdn.discordapp.com/avatars/603948445362946084/a_f61398e073d78ae104e32b0517c891c3.gif")
         .setTimestamp();
     leaderboard.forEach(u => {
-        embed.addField(`${u.position}. ${client.users.cache.get(u.id) ? client.users.cache.get(u.id).tag : "Unknown#0000"}`, `${u.money} 🪙`);
+        embed.addField(`${u.position}. ${client.users.cache.get(u.id) ? client.users.cache.get(u.id).tag : "Can't Load#0000"}`, `${u.money} 🪙`);
     });
     return message.channel.send(embed);
 }

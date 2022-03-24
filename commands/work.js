4502a3e901955bc70b45b60db6baf724df5f6709
meflems <89581469+meflems@users.noreply.github.com>
2022-03-24 19:21:22 +0530

    update work.js

--- commands/work.js
+++ commands/work.js
@@ -1,8 +1,8 @@
 module.exports.execute = async (client, message, args) => {
-    let amount = Math.floor(Math.random() * 1500) + 1000;
+    let amount = Math.floor(Math.random() * 170) + 20;
     let work = client.eco.work(client.ecoAddUser, amount);
     if (work.onCooldown) return message.reply(`You are tired rn. Come back after ${work.time.minutes} minutes & ${work.time.seconds} seconds to work again.`);
-    else return message.reply(`You worked as **${work.workedAs}** and earned **${work.amount}** 💸. Now you have **${work.after}** 💸.`);
+    else return message.reply(`You worked as **${work.workedAs}** and earned **${work.amount}** ��. Now you have **${work.after}** ��.`);
 };
 
 module.exports.help = {

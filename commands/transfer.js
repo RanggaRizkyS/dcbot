12a389373a30358c7cd0743bf96a9a2af73d6af4
meflems <89581469+meflems@users.noreply.github.com>
2022-03-24 15:42:53 +0530

    update transfer.js

--- commands/transfer.js
+++ commands/transfer.js
@@ -1,12 +1,12 @@
 exports.execute = async (client, message, args) => {
   let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) 
   let authordata = client.eco.fetchMoney(message.author.id) 
-  if (!member) return message.channel.send('Please mention the person or give their ID') 
+  if (!member) return message.channel.send('Mention or give thier ID!') 
   let amount = args[1]
-  if (!amount || isNaN(amount)) return message.channel.send('Please enter a valid amount to transfer') 
-  if(authordata.amount < amount) return message.channel.send('Looks like you don\'t have that much money') 
+  if (!amount || isNaN(amount)) return message.channel.send('Enter valid amount!') 
+  if(authordata.amount < amount) return message.channel.send("Bruh, you don'n have that much money") 
   await client.eco.transfer(message.author.id, member.id, amount) 
-  return message.channel.send(`You have successfully transferred 💸**${amount}** to ** ${member.user.tag}**.`)
+  return message.channel.send(`Sucess to transfer ${amount} to ${member.user.tag}`)
 }
 exports.help = {
   name: "transfer",

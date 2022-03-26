24195cf06337eca99a88d1bbc729214b13a5034b
meflems <89581469+meflems@users.noreply.github.com>
2022-03-26 23:39:37 +0530

    update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = (client) => {
     console.log(`${client.user.tag} is online!`);
-    client.user.setActivity("!help - new web https://acroga.xyz", {
+    client.user.setActivity("!help - web https://acroga.xyz", {
       type: "STREAMING",
       url: "https://acroga.xyz"
     });

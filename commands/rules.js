013de2e3b80c161dad3778b03090ed09132f1d62
meflems <89581469+meflems@users.noreply.github.com>
2022-04-04 04:31:26 +0530

    update rules.js

--- commands/rules.js
+++ commands/rules.js
@@ -1,10 +1,7 @@
 const { MessageEmbed } = require("discord.js");
 
 exports.execute = (client, message, args) => {
-    let pingRandom = Math.floor(Math.random() * 20) + 10;
-    let pingClient = Math.floor(Math.random() * 23) + 20;
-    let gatewayLatency = Math.floor(client.ws.ping);
-        const embed = new MessageEmbed()
+	    let embed = new MessageEmbed()
             .setTitle("Frownmc Rules")
             .setDescription("`[1.] Jangan berkata kasar\n[2.] Jika membeli barang, harus mematuhi persyaratan\n[3.] Metode pembayaran hanya tersedia di <#960309389061939200>, Selain itu tidak\n[4.] Di larang keras untuk promosi\n[5.] Di larang spam\n[6.] Di larang kirim foto/video Pornografi/18+\n[7.] Di larang scam`")
             .setColor("#8F00FF")

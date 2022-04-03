a7a4c5f4d937c0cdff95fb3e15b92bed994dc9e7
meflems <89581469+meflems@users.noreply.github.com>
2022-04-04 04:29:36 +0530

    update rules.js

--- commands/rules.js
+++ commands/rules.js
@@ -6,7 +6,7 @@ exports.execute = (client, message, args) => {
     let gatewayLatency = Math.floor(client.ws.ping);
         const embed = new MessageEmbed()
             .setTitle("Frownmc Rules")
-            .setDescription("`[1.] Jangan berkata kasar\n[2.] Jika membeli barang, harus mematuhi persyaratan\n[3.] Metode pembayaran hanya tersedia di <@960309389061939200>, Selain itu tidak\n[4.] Di larang keras untuk promosi\n[5.] Di larang spam\n[6.] Di larang kirim foto/video Pornografi/18+\n[7.] Di larang scam`")
+            .setDescription("`[1.] Jangan berkata kasar\n[2.] Jika membeli barang, harus mematuhi persyaratan\n[3.] Metode pembayaran hanya tersedia di <#960309389061939200>, Selain itu tidak\n[4.] Di larang keras untuk promosi\n[5.] Di larang spam\n[6.] Di larang kirim foto/video Pornografi/18+\n[7.] Di larang scam`")
             .setColor("#8F00FF")
             .setTimestamp();
         message.channel.send(embed);

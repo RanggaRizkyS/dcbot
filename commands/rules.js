8d9f47ebc4c7a7ea47b4878fbaddf2b8ab30daf0
meflems <89581469+meflems@users.noreply.github.com>
2022-04-04 04:35:07 +0530

    update rules.js

--- commands/rules.js
+++ commands/rules.js
@@ -3,7 +3,7 @@ const { MessageEmbed } = require("discord.js");
 exports.execute = (client, message, args) => {
 	    let embed = new MessageEmbed()
             .setTitle("Frownmc Rules")
-            .setDescription("`[1.] Jangan berkata kasar\n[2.] Jika membeli barang, harus mematuhi persyaratan\n[3.] Metode pembayaran hanya tersedia di <#960309389061939200>, Selain itu tidak\n[4.] Di larang keras untuk promosi\n[5.] Di larang spam\n[6.] Di larang kirim foto/video Pornografi/18+\n[7.] Di larang scam`")
+            .setDescription("`[1.] Jangan berkata kasar\n[2.] Jika membeli barang, harus mematuhi persyaratan\n[3.] Metode pembayaran hanya tersedia di Payment, Selain itu tidak tersedia!\n[4.] Di larang keras untuk promosi\n[5.] Di larang spam\n[6.] Di larang kirim foto/video Pornografi/18+\n[7.] Di larang scam`")
             .setColor("#8F00FF")
             .setTimestamp();
         message.channel.send(embed);

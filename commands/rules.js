2e8952de0422a73683a3f7e3c0cb74bc1f80a315
meflems <89581469+meflems@users.noreply.github.com>
2022-04-04 04:45:47 +0530

    update rules.js

--- commands/rules.js
+++ commands/rules.js
@@ -3,7 +3,7 @@ const { MessageEmbed } = require("discord.js");
 exports.execute = (client, message, args) => {
 	    let embed = new MessageEmbed()
             .setTitle("Frownmc Payment")
-            .setDescription("`> [1.] Gopay\n+62 88214812908\n> [2.] Dana\n+62 88214812908\n> [3.] Pulsa Smartfren +5k\n+62 88214812908\n================\nNote: Pembayaran Selain Itu Tidak Di Terima!`")
+            .setDescription("> **[1.] Gopay**\n+62 88214812908\n> **[2.] Dana**\n+62 88214812908\n> **[3.] Pulsa Smartfren +5k**\n+62 88214812908\n================\n**Note: Pembayaran Selain Itu Tidak Di Terima!**")
             .setColor("#8F00FF")
             .setTimestamp();
         message.channel.send(embed);

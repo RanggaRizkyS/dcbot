e668afb3d42eac68eb9722e20fa081b7f5372df5
meflems <89581469+meflems@users.noreply.github.com>
2022-04-04 04:24:04 +0530

    update rules.js

--- commands/rules.js
+++ commands/rules.js
@@ -4,14 +4,12 @@ exports.execute = (client, message, args) => {
     let pingRandom = Math.floor(Math.random() * 20) + 10;
     let pingClient = Math.floor(Math.random() * 23) + 20;
     let gatewayLatency = Math.floor(client.ws.ping);
-    message.channel.send("Pinging...").then(m => {
-        const trip = Math.floor(m.createdTimestamp - message.createdTimestamp);
         const embed = new MessageEmbed()
             .setTitle("Frownmc Rules")
             .setDescription(`[1.] Jangan berkata kasar\n[2.] Jika membeli barang, harus mematuhi persyaratan\n[3.] Metode pembayaran hanya tersedia di <@960309389061939200>, Selain itu tidak\n[4.] Di larang keras untuk promosi\n[5.] Di larang spam\n[6.] Di larang kirim foto/video Pornografi/18+\n[7.] Di larang scam`)
             .setColor("#8F00FF")
             .setTimestamp();
-        m.edit(embed);
+        message.channel.send(embed);
     });
 }
 

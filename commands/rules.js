7e7e1cd3b54ed418d9c4fcf2096d76999dd2ddbc
meflems <89581469+meflems@users.noreply.github.com>
2022-04-04 12:15:40 +0530

    update rules.js

--- commands/rules.js
+++ commands/rules.js
@@ -3,7 +3,7 @@ const { MessageEmbed } = require("discord.js");
 exports.execute = (client, message, args) => {
 	    let embed = new MessageEmbed()
             .setTitle("Frownmc Cheap Hosting")
-            .setDescription("`Paket 1 | 10.000 / Bulan\nRam: 1GB\nDisk: 5GB\nCpu: 100 Intel Xeon 8vCPU\nRegion: Singapure 🇸🇬`")
+            .setDescription("`Paket 1 | 10.000 / Bulan\nRam: 1GB\nDisk: 5GB\nCpu: 100 Intel Xeon 8vCPU\nRegion: Singapure 🇸🇬\n\nPaket 2 | 20.000 / Bulan\nRam: 2GB\nDisk: 15GB\nCpu: 120 Intel Xeon 8vCPU`")
             .setColor("#8F00FF")
             .setTimestamp();
         message.channel.send(embed);

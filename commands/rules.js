f91e491b7804db2a4de2e973b768afd52f9b3783
meflems <89581469+meflems@users.noreply.github.com>
2022-04-04 12:23:06 +0530

    update rules.js

--- commands/rules.js
+++ commands/rules.js
@@ -3,7 +3,7 @@ const { MessageEmbed } = require("discord.js");
 exports.execute = (client, message, args) => {
 	    let embed = new MessageEmbed()
             .setTitle("Frownmc Cheap Hosting")
-            .setDescription("`Paket 1 | 10.000 / Bulan\nRam: 1GB\nDisk: 5GB\nCpu: 100 Intel Xeon 8vCPU\nRegion: Singapure 🇸🇬\n\nPaket 2 | 20.000 / Bulan\nRam: 2GB\nDisk: 15GB\nCpu: 120 Intel Xeon 8vCPU\nRegion: Singapure 🇸🇬\n\nPaket 3 | 40.000 / Bulan\nRam: 4GB\nDisk: 30GB\nCpu: 150 Intel Xeon 8vCPU\nRegion: Singapure 🇸🇬 \n\nPaket 4 | 50.000 / Bulan\nRam: 6GB\nDisk: 35GB\nCpu: 150 Intel Xeon 8vCPU\nRegion: Singapure 🇸🇬`")
+            .setDescription("`Paket 1 | 10.000 / Bulan\nRam: 1GB\nDisk: 5GB\nCpu: 100 Intel Xeon 8vCPU\nRegion: Singapure 🇸🇬\n\nPaket 2 | 20.000 / Bulan\nRam: 2GB\nDisk: 15GB\nCpu: 120 Intel Xeon 8vCPU\nRegion: Singapure 🇸🇬\n\nPaket 3 | 40.000 / Bulan\nRam: 4GB\nDisk: 25GB\nCpu: 150 Intel Xeon 8vCPU\nRegion: Singapure 🇸🇬 \n\nPaket 4 | 50.000 / Bulan\nRam: 6GB\nDisk: 30GB\nCpu: 150 Intel Xeon 8vCPU\nRegion: Singapure 🇸🇬`")
             .setColor("#8F00FF")
             .setTimestamp();
         message.channel.send(embed);
